f5fcec306f54ce88413035c2914bc30a32e53ade
Your Name <your@example.com>
2016-05-26 10:43:32 -0400

    initial setup

---
+++ compare points code_eval/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compare_points_code_eval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "0 1 0 5";
+            string[] lost = line.Split();
+
+
+            int xHere = int.Parse(lost[0]);  //initializing our two x, y, coordinates
+            int yHere = int.Parse(lost[1]);
+            int xThere = int.Parse(lost[2]);
+            int yThere = int.Parse(lost[3]);
+
+            string direction = ""; // initializing string we will return to CodeEval
+
+            //setting string direction based on coordinate math
+
+            if (xHere == xThere && yHere == yThere)
+                direction = "here";
+
+            if (xHere == xThere && yHere < yThere)
+                direction = "N";
+
+            if (xHere == xThere && yHere > yThere)
+                direction = "S";
+
+            if (xHere > xThere && yHere == yThere)
+                direction = "W";
+
+            if (xHere < xThere && yHere == yThere)
+                direction = "E";
+
+            if (xHere < xThere && yHere < yThere)
+                direction = "NE";
+
+            if (xHere < xThere && yHere > yThere)
+                direction = "SE";
+
+            if (xHere > xThere && yHere < yThere)
+                direction = "SE";
+
+            if (xHere > xThere && yHere < yThere)
+                direction = "NW";
+
+            if (xHere > xThere && yHere > yThere)
+                direction = "SW";
+            //Returning direction to CodeEval
+            Console.WriteLine(direction);
+        }
+    }
+}
